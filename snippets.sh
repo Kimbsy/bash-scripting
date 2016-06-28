216d83cb5b88d37d5909d29c11a4db37675346d6
Kimbsy <dave.kimber@simitive.com>
2016-06-28 12:34:52 +0100

    snippets for checking number of function args and return values

--- snippets.sh
+++ snippets.sh
@@ -12,4 +12,19 @@ if ! grep -q $SEARCH_TERM $FILE_NAME; then
     echo "do stuff"
 fi
 
+# Check the number of arguments passed to a function.
+ARG_COUNT=1
+if [ $# -ne $ARG_COUNT]; then
+    echo "Usage: $FUNCNAME arg"
+fi
 
+# Check the return value of a function.
+#
+# There may be a better way of doing this.
+testfunc() {
+    return 4
+}
+testfunc
+if [[ $? -eq 4 ]]; then
+    echo "returned 4"
+fi
