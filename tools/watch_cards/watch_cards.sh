1d05bb6e6dd5828651a68f6739a93ed02c18f7b7
Dave Kimber <dkimber@gresham-computing.com>
2016-12-23 10:40:35 +0000

    printing prices based on min/max history, adding color printing to snippets

--- tools/watch_cards/watch_cards.sh
+++ tools/watch_cards/watch_cards.sh
@@ -5,7 +5,7 @@
 #******************************************************************************#
 # Replace spaces with '+' characters for  URL and price data file name.
 #******************************************************************************#
-url_encode() {
+encode() {
     echo ${1// /+}
 }
 
@@ -13,7 +13,7 @@ url_encode() {
 # Construct appropriate MTGGoldfish URL.
 #******************************************************************************#
 get_url() {
-    echo "https://www.mtggoldfish.com/price/$(url_encode "$1")/$(url_encode "$2")"
+    echo "https://www.mtggoldfish.com/price/$(encode "$1")/$(encode "$2")"
 }
 
 #******************************************************************************#
@@ -38,7 +38,7 @@ get_price() {
 #******************************************************************************#
 save_price() {
     current_price=$3
-    file_name="price_data/$(url_encode "$1")--$(url_encode "$2")"
+    file_name="price_data/$(encode "$1")--$(encode "$2")"
 
     # If this is a new card, the current price is min and max.
     if [ ! -f "$file_name" ]; then
@@ -66,14 +66,40 @@ save_price() {
 # Print the card price details.
 #******************************************************************************#
 print_card_price() {
-    echo "$1 price:"
-    echo "$2"
-    echo ""
+    NC='\033[0m'
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+
+    cur_c=$NC
+    min_c=$GREEN
+    max_c=$RED
+
+    # Get min and max prices from file.
+    IFS=';' read -r -a min_max_prices <<< $(cat $file_name)
+    min_price="${min_max_prices[0]}"
+    max_price="${min_max_prices[1]}"
+
+    # Float string comparison to determine new colours.
+    if (( $(echo "$current_price == $max_price" | bc -l) )); then
+        cur_c=$RED
+    fi
+    if (( $(echo "$current_price == $min_price" | bc -l) )); then
+        cur_c=$GREEN
+    fi
+    if (( $(echo "$min_price == $max_price" | bc -l) )); then
+        max_c=$GREEN
+    fi
+
+    echo "$2 price:"
+    printf "    ${cur_c}$3${NC}\n"
+    printf "MIN ${min_c}$min_price${NC}\n"
+    printf "MAX ${max_c}$max_price${NC}\n\n"
 }
 
 
+
+
 while read -r card_line; do
-    # Split the card line into set name and card name.
     IFS=';' read -r -a details <<< "$card_line"
     set_name=${details[0]}
     card_name=${details[1]}
@@ -81,7 +107,5 @@ while read -r card_line; do
     price=$(get_price "$set_name" "$card_name")
     save_price "$set_name" "$card_name" "$price"
 
-    print_card_price "$card_name" "$price"
+    print_card_price "$set_name" "$card_name" "$price"
 done < watch_list
-
-
