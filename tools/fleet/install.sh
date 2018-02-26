8b997776c615f216decfe493e9a124aa7b6244ca
Dave Kimber <dkimber@greshamtech.com>
2018-02-26 15:13:50 +0000

    fixing permission errors

--- tools/fleet/install.sh
+++ tools/fleet/install.sh
@@ -14,10 +14,10 @@ sudo ln -s "$(pwd)/$name" "$binary"
 opt_dir="/opt/$(whoami)/bash/$name"
 if [ ! -f "$opt_dir" ]; then
     sudo mkdir -p "$opt_dir"
-    sudo chmod -R 755 "/opt/$(whoami)"
 fi
 
-# Copy parsers for individual sites.
+# Copy list of ship names.
 sudo cp "$(pwd)/eve_online_ships" "$opt_dir/"
+sudo chmod -R 755 "/opt/$(whoami)"
 
 echo "Installation complete."
